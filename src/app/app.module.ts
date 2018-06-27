4b5dd9cd0bab4f27e524a1218f2f5273ea2b9301
roveroniandrea <roveroniandrea@gmail.com>
2018-06-27 17:44:02 +0200

    Routing menu link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCheckboxModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule} from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { Routes, RouterModule } from '@angular/router'
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { LayoutModule } from '@angular/cdk/layout';
+import { FormsModule } from '@angular/forms';
+import { ContattiComponent } from './contatti/contatti.component';
+import { HomeComponent } from './home/home.component';
+import { CatalogoLibriComponent } from './catalogo-libri/catalogo-libri.component';
+
+const appRoutes: Routes =[
+  {path: "", component: HomeComponent},
+  {path: "catalogo", component: CatalogoLibriComponent},
+  {path: "contatti", component: ContattiComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainMenuComponent
+    MainMenuComponent,
+    ContattiComponent,
+    HomeComponent,
+    CatalogoLibriComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +39,9 @@ import { LayoutModule } from '@angular/cdk/layout';
     MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
