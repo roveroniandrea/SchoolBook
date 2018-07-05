2555bd72d951e63b32a93e0a87d72c0913ea3dea
roveroniandrea <roveroniandrea@gmail.com>
2018-07-05 16:54:21 +0200

    inserito reactive form in new-book

--- src/app/nuovo-libro/nuovo-libro.component.ts
+++ src/app/nuovo-libro/nuovo-libro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-nuovo-libro',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nuovo-libro.component.css']
 })
 export class NuovoLibroComponent implements OnInit {
-
+  uploadForm : FormGroup;
+  patternPrezzo = "^(?!0\.00)\d{1,3}(,\d{3})*(\.\d\d)?$";
   constructor() { }
 
   ngOnInit() {
+    this.uploadForm = new FormGroup({
+      "titolo" : new FormControl("",Validators.required),
+      "isbn" : new FormControl(null,Validators.required),
+      "prezzo" : new FormControl("00.00",Validators.required), //inserire pattern moneta
+      "descrizione": new FormControl("",Validators.required)
+    })
   }
 
+  submitForm(){
+    console.log(this.uploadForm.value)
+  }
+
+  annullaForm(){
+    
+  }
 }
