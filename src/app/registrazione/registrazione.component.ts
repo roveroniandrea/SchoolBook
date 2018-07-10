59381c7b8655751edb171eeac65a06e52bfe4fdf
Nicola Valentini <n.valen@hotmail.it>
2018-07-10 14:10:38 +0200

    fix registrazione html base

--- src/app/registrazione/registrazione.component.ts
+++ src/app/registrazione/registrazione.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrazione',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrazione.component.css']
 })
 export class RegistrazioneComponent implements OnInit {
+  accountForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.accountForm = new FormGroup({
+      "nome": new FormControl("", Validators.required),
+      "cognome": new FormControl(""),
+      "mail": new FormControl("", [Validators.required, Validators.email]),
+      "telefono": new FormControl(""),
+      "scuola": new FormControl("", Validators.required),
+      "password": new FormControl("", Validators.required),
+    })
   }
 
 }
