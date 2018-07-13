4fd8c4f6fcba9569d5273ccbe5d42d57ff2fcd80
Nicola Valentini <n.valen@hotmail.it>
2018-07-13 18:01:38 +0200

    review login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,41 +2,43 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { UserService } from '../servizi/utente.service';
-import * as firebase from "firebase";
+//import * as firebase from "firebase";
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
+  /* Variabili */
   loginForm: FormGroup;
   error: Error;
 
   constructor(private autenticazione: AngularFireAuth,
-    private route: Router,
-    private user: UserService) { }
+    private route: Router) { }
 
   ngOnInit() {
+    /* Creo il form */ 
     this.loginForm = new FormGroup({
       "mail": new FormControl("", Validators.required),
       "password": new FormControl("", Validators.required),
     })
   }
 
+  /* Controllo i valori inseriti */
   login() {
     this.autenticazione.auth.signInWithEmailAndPassword(this.loginForm.value.mail, this.loginForm.value.password)
-      .catch(err => {
-        console.log("Login Errato.", this.user.utente)
-        this.error = err;
+      .catch(error => {
+        this.error = error;
       })
       .then(result => {
         if (result) {
-          console.log("Login Effettuato.", this.user.utente)
           this.route.navigate(["/"], { queryParams: { "utenteLoggato": 1 } });
         }
       })
   }
+
 /* GOOGLE login
   google() {
     this.autenticazione.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider)
@@ -47,5 +49,5 @@ export class LoginComponent implements OnInit {
       console.log(user);
     })
   }
-  */
+*/
 }
