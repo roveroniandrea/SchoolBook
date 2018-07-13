e0ea4f5ddece01a9cbe78d4d3098ca73606c180f
roveroniandrea <roveroniandrea@gmail.com>
2018-07-13 18:16:29 +0200

    welcome mail Funziona!

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,6 +1,7 @@
 'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
 //import * as functions from "firebase-functions";
-import functions = require('firebase-functions');
+const functions = require("firebase-functions");
 //import { nodemailer } from "nodemailer";
 const nodemailer = require('nodemailer');
 // // Start writing Firebase Functions
@@ -13,38 +14,35 @@ const APP_NAME = 'School Book';
 const gmailEmail = functions.config().gmail.email;
 const gmailPassword = functions.config().gmail.password;
 const mailTransport = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: gmailEmail,
-    pass: gmailPassword,
-  },
+    service: 'gmail',
+    auth: {
+        user: gmailEmail,
+        pass: gmailPassword,
+    },
 });
 // [START sendWelcomeEmail]
 /**
  * Sends a welcome email to new user.
  */
 // [START onCreateTrigger]
-exports.sendWelcomeEmail = functions.auth.user().onCreate((user : any) => {
+exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
     // [END onCreateTrigger]
-      // [START eventAttributes]
-      const email = user.email; // The email of the user.
-      // [END eventAttributes]
-    
-      return sendWelcomeEmail(email);
-    });
-    // [END sendWelcomeEmail]
-
-
+    // [START eventAttributes]
+    const email = user.email; // The email of the user.
+    // [END eventAttributes]
+    return sendWelcomeEmail(email);
+});
+// [END sendWelcomeEmail]
 // Sends a welcome email to the given user.
 function sendWelcomeEmail(email) {
     const mailOptions = {
-      from: `${APP_NAME} <noreply@firebase.com>`,
-      to: email,
-      subject :`Benvenuto su ${APP_NAME}!`,
-      text : `Ciao! Benvenuto su ${APP_NAME}, ora potrai vendere i tuoi libri e comprarne altri!`
+        from: `${APP_NAME} <noreply@firebase.com>`,
+        to: email,
+        subject: `Welcome to ${APP_NAME}!`,
+        text: `Ciao! Benvenuto su ${APP_NAME}, ora potrai vendere i tuoi libri e comprarne altri!`
     };
-  
     return mailTransport.sendMail(mailOptions).then(() => {
-      return console.log('Email di benvenuto spedita a: ', email);
+        return console.log('Email di benvenuto spedita a: ', email);
     });
-  }
+}
+//# sourceMappingURL=index.js.map
